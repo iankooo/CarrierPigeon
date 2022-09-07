9bd06733025bd24d436cb7eafad8782c21276650
ENDAVA\ischipala <ianko.schipala@endava.com>
2022-09-07 11:19:54 +0300

    create profile screen done

--- app/src/main/java/com/example/carrier_pigeon/features/createProfile/CreateProfileFragment.kt
+++ app/src/main/java/com/example/carrier_pigeon/features/createProfile/CreateProfileFragment.kt
@@ -5,15 +5,39 @@ import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.example.carrier_pigeon.R
 import com.example.carrier_pigeon.app.common.BaseFragment
+import com.example.carrier_pigeon.app.utils.shortToast
+import com.example.carrier_pigeon.data.enums.SharedPrefsWrapper
 import com.example.carrier_pigeon.databinding.FragmentCreateProfileBinding
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class CreateProfileFragment : BaseFragment(R.layout.fragment_create_profile) {
     private val binding by viewBinding(FragmentCreateProfileBinding::bind)
 
+    @Inject
+    lateinit var sharedPrefsWrapper: SharedPrefsWrapper
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.saveProfileBtn.setOnClickListener {
-            findNavController().navigate(CreateProfileFragmentDirections.createProfileToPigeon())
+            if (binding.phoneNumberEt.text.isEmpty()) {
+                shortToast(getString(R.string.phone_number_cannot_be_empty))
+            } else if (binding.firstNameEt.text.isEmpty()) {
+                shortToast(getString(R.string.first_name_cannot_be_empty))
+            } else if (binding.lastNameEt.text.isEmpty()) {
+                shortToast(getString(R.string.last_name_cannot_be_empty))
+            } else if (binding.homeAddressEt.text.isEmpty()) {
+                shortToast(getString(R.string.home_address_cannot_be_empty))
+            } else {
+                with(sharedPrefsWrapper) {
+                    savePhoneNumber(binding.phoneNumberEt.text.toString())
+                    saveFirstName(binding.firstNameEt.text.toString())
+                    saveLastName(binding.lastNameEt.text.toString())
+                    saveHomeAddress(binding.homeAddressEt.text.toString())
+                }
+                findNavController().navigate(CreateProfileFragmentDirections.createProfileToPigeon())
+            }
         }
     }
 }
