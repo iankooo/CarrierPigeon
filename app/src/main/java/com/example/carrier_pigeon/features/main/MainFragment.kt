dff234ee39fa71dd6e5c108078c39db88dbae1d6
ENDAVA\ischipala <ianko.schipala@endava.com>
2022-09-23 12:18:57 +0300

    start working on families

--- app/src/main/java/com/example/carrier_pigeon/features/main/MainFragment.kt
+++ app/src/main/java/com/example/carrier_pigeon/features/main/MainFragment.kt
@@ -2,16 +2,11 @@ package com.example.carrier_pigeon.features.main
 
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.view.ViewCompat
 import androidx.navigation.fragment.findNavController
-import androidx.viewpager.widget.ViewPager
 import com.example.carrier_pigeon.R
 import com.example.carrier_pigeon.app.common.BaseFragment
 import com.example.carrier_pigeon.data.enums.SharedPrefsWrapper
 import com.example.carrier_pigeon.databinding.FragmentMainBinding
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,9 +21,6 @@ val animalsArray = arrayOf(
 class MainFragment : BaseFragment(R.layout.fragment_main) {
     @Inject
     lateinit var sharedPrefsWrapper: SharedPrefsWrapper
-
-    var tabLayout: TabLayout? = null
-    var viewPager: ViewPager? = null
 
     private val binding by viewBinding(FragmentMainBinding::bind)
 
